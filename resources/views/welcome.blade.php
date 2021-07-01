c5380fea40be0bf8e24fba1043483b91c9ff53a8
giftTheProgrammer <m.g.moletsane@gmail.com>
2021-07-01 19:54:52 +0200

    made a layout for index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Artworks</h1>
-    @if(count($artworks) > 0)
-    	@foreach($artworks as $artwork)
-	        <ul>
-	            <li><img src="{{URL::asset('storage/photos/'.$artwork->artwork_thumbnail)}}" height="450" width="320"></li>
-	        </ul>
-        @endforeach
-    @endif
+    <h1 class="heading">Artworks</h1>
+    <div class="display-body">
+    	@if(count($artworks) > 0)
+	    	@foreach($artworks as $artwork)
+		        <ul>
+		            <li class="arts">
+		            	<div class="item">
+		            		<img src="{{URL::asset('storage/photos/'.$artwork->artwork_thumbnail)}}" height="360" width="240" />
+		            		<div class="item-details">
+		            			<p>Title:</p>
+		            			<h2>{{ $artwork->title }}</h2>
+		            			<p>Type:</p>
+		            			<h3>{{ $artwork->artwork_type }}</h3>
+		            			<h3><strong>R{{ $artwork->artwork_price }}</strong></h3>
+		            			<button class="btn btn-primary">add to cart</button>
+		            		</div>
+		            	</div>
+		            </li>
+		        </ul>
+	        @endforeach
+    	@endif
+    </div>
+    
 @endsection
