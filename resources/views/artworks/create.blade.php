71df4922d3e043dcf2ad4296afb1292893345e61
giftTheProgrammer <m.g.moletsane@gmail.com>
2021-07-02 20:50:39 +0200

    Improved the layout some more

--- resources/views/artworks/create.blade.php
+++ resources/views/artworks/create.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 	<h1>Sumbit an Artwork</h1>
+
+	@if($errors->any())
+		<div class="alert alert-danger">
+			<ul>
+				@foreach($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
 	{!! Form::open(['action' => 'ArtworksController@store', 'method' => 'POST', 'id' => 'form-edit', 'enctype' => 'multipart/form-data', 'file' => true]) !!}
 		{!! Form::label('artwork_type', 'Type') !!}<br />
 		{!! Form::select('artwork_type', ['music' => 'Music', 'book' => 'Book']) !!}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
 		        <ul>
 		            <li class="arts">
 		            	<div class="item">
-		            		<img src="{{URL::asset('storage/photos/'.$artwork->artwork_thumbnail)}}" height="360" width="240" />
+		            		<img src="{{URL::asset('storage/photos/'.$artwork->artwork_thumbnail)}}" height="320" width="240" />
 		            		<div class="item-details">
 		            			<p>Title:</p>
 		            			<h2>{{ $artwork->title }}</h2>
 		            			<p>Type:</p>
 		            			<h3>{{ $artwork->artwork_type }}</h3>
-		            			<h3><strong>R{{ $artwork->artwork_price }}</strong></h3>
+		            			<h3 id="the-money"><strong>R{{ $artwork->artwork_price }}</strong></h3>
 		            			<button class="btn btn-primary">add to cart</button>
 		            		</div>
 		            	</div>
