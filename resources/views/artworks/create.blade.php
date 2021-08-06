fd4e22f0b7904b2d0de43f3038a189ffeff027fd
giftTheProgrammer <m.g.moletsane@gmail.com>
2021-08-06 20:46:05 +0200

    Improved the layout of the Createe and Edit views

--- resources/views/artworks/create.blade.php
+++ resources/views/artworks/create.blade.php
@@ -1,55 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Sumbit an Artwork</h1>
+	<div class="container">
+		<h1>Sumbit an Artwork</h1>
 
-	@if($errors->any())
-		<div class="alert alert-danger">
-			<ul>
-				@foreach($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		</div>
-	@endif
-	{!! Form::open(['action' => 'ArtworksController@store', 'method' => 'POST', 'id' => 'form-edit', 'enctype' => 'multipart/form-data', 'file' => true]) !!}
-		<div class="form-group">
-			{{ Form::label('artwork_type', 'Type') }}<br />
-			{{ Form::select('artwork_type', ['music' => 'Music', 'book' => 'Book'], null, ['class' => 'form-control']) }}
-		</div>
-		
-		<br />
-		<div class="form-group">
-			{{ Form::label('title', 'Title') }}<br />
-			{{ Form::text('title', '', ['class' => 'form-control']) }}
-		</div>
-		<br />
+		@if($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+		{!! Form::open(['action' => 'ArtworksController@store', 'method' => 'POST', 'id' => 'form-edit', 'enctype' => 'multipart/form-data', 'file' => true]) !!}
+			<div class="form-group">
+				{{ Form::label('artwork_type', 'Type') }}<br />
+				{{ Form::select('artwork_type', ['music' => 'Music', 'book' => 'Book'], null, ['class' => 'form-control']) }}
+			</div>
+			
+			<br />
+			<div class="form-group">
+				{{ Form::label('title', 'Title') }}<br />
+				{{ Form::text('title', '', ['class' => 'form-control']) }}
+			</div>
+			<br />
 
-		<div class="form-group">
-			{{ Form::label('artist', 'Artist') }}
-			{{ Form::text('artist', '', ['class' =>  'form-control']) }}
-		</div>
-		<br />
-		
-		<div class="form-group">
-			{{ Form::label('price', 'Price') }}<br />
-			{{ Form::text('price', '', ['class' => 'form-control']) }}
-		</div>
-		<br />
-		<div class="custom-file">
-			{{ Form::file('thumbnail_dir', ['class' => 'custom-file-input']) }}
-			{{ Form::label('thumbnail_dir', 'Thumbnail', ['class' => 'custom-file-label']) }}
-		</div>
-		<br /><br />
-		<div class="custom-file">
-			{{ Form::file('audiofile_dir', ['class' => 'custom-file-input']) }}
-			{{ Form::label('audiofile_dir', 'Upload Your Song', ['class' => 'custom-file-label']) }}
-		</div>
-		<br /><br />
-		<div class="form-group">
-			{{ Form::submit('Submit', ['class' => 'btn btn-primary', 'id' => 'btn']) }}
-		</div>
-	{!! Form::close() !!}
+			<div class="form-group">
+				{{ Form::label('artist', 'Artist') }}
+				{{ Form::text('artist', '', ['class' =>  'form-control']) }}
+			</div>
+			<br />
+
+			<div class="form-group">
+				{{ Form::label('price', 'Price') }}<br />
+				{{ Form::text('price', '', ['class' => 'form-control']) }}
+			</div>
+			<br />
+			<div class="custom-file">
+				{{ Form::file('thumbnail_dir', ['class' => 'custom-file-input']) }}
+				{{ Form::label('thumbnail_dir', 'Thumbnail', ['class' => 'custom-file-label']) }}
+			</div>
+			<br /><br />
+			<div class="custom-file">
+				{{ Form::file('audiofile_dir', ['class' => 'custom-file-input']) }}
+				{{ Form::label('audiofile_dir', 'Upload Your Song', ['class' => 'custom-file-label']) }}
+			</div>
+			<br /><br />
+			<div class="form-group">
+				{{ Form::submit('Submit', ['class' => 'btn btn-primary', 'id' => 'btn']) }}
+			</div>
+		{!! Form::close() !!}
+	</div>
+	
 
 	<script type="text/javascript">
 		$(document).ready(function(){
--- resources/views/artworks/edit.blade.php
+++ resources/views/artworks/edit.blade.php
@@ -1,62 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-	<a href="/home" class="btn btn-outline-dark">Back</a>
-	<h1>Edit Song</h1>
+	<div class="container">
+		<a href="/home" class="btn btn-outline-dark">Back</a>
+		<h1>Edit Song</h1>
 
-	@if($errors->any())
-		<div class="alert alert-danger">
-			<ul>
-				@foreach($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		</div>
-	@endif
+		@if($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
 
-	{!! Form::open(['action' => ['ArtworksController@update', $artwork->id], 'method' => 'POST', 'enctype' => 'multipart/form-data', 'file' => true]) !!}
-		<div class="form-group">
-			{{ Form::label('artwork_type', 'Type') }}
-			{{ Form::select('artwork_type', ['music' => 'Music', 'book' => 'Book'], null, ['class' => 'form-control']) }}
-		</div>
-		<br /><br />
+		{!! Form::open(['action' => ['ArtworksController@update', $artwork->id], 'method' => 'POST', 'enctype' => 'multipart/form-data', 'file' => true]) !!}
+			<div class="form-group">
+				{{ Form::label('artwork_type', 'Type') }}
+				{{ Form::select('artwork_type', ['music' => 'Music', 'book' => 'Book'], null, ['class' => 'form-control']) }}
+			</div>
+			<br /><br />
 
-		<div class="form-group">
-			{{ Form::label('title', 'Title') }}
-			{{ Form::text('title', $artwork->title, ['class' => 'form-control']) }}
-		</div>
-		<br /><br />
+			<div class="form-group">
+				{{ Form::label('title', 'Title') }}
+				{{ Form::text('title', $artwork->title, ['class' => 'form-control']) }}
+			</div>
+			<br /><br />
 
-		<div class="form-group">
-			{{ Form::label('artist', 'Artist') }}
-			{{ Form::text('artist', $artwork->artist, ['class' =>  'form-control']) }}
-		</div>
-		<br />
+			<div class="form-group">
+				{{ Form::label('artist', 'Artist') }}
+				{{ Form::text('artist', $artwork->artist, ['class' =>  'form-control']) }}
+			</div>
+			<br />
 
-		<div class="form-group">
-			{{ Form::label('price', 'Price') }}
-			{{ Form::text('price', $artwork->artwork_price, ['class' => 'form-control']) }}
-		</div>
-		<br /><br />
+			<div class="form-group">
+				{{ Form::label('price', 'Price') }}
+				{{ Form::text('price', $artwork->artwork_price, ['class' => 'form-control']) }}
+			</div>
+			<br /><br />
 
-		<div class="custom-file">
-			{{ Form::label('thumbnail_dir', 'Thumbnail', ['class' => 'custom-file-label']) }}<br />
-			{{ Form::file('thumbnail_dir', ['class' => 'custom-file-input']) }}
-		</div>
-		<br /><br />
+			<div class="custom-file">
+				{{ Form::label('thumbnail_dir', 'Thumbnail', ['class' => 'custom-file-label']) }}<br />
+				{{ Form::file('thumbnail_dir', ['class' => 'custom-file-input']) }}
+			</div>
+			<br /><br />
 
-		<div class="custom-file">
-			{{ Form::label('audiofile_dir', 'Upload Your Song', ['class' => 'custom-file-label']) }}<br />
-			{{ Form::file('audiofile_dir', ['class' => 'custom-file-input']) }}
-		</div>
-		<br /><br />
+			<div class="custom-file">
+				{{ Form::label('audiofile_dir', 'Upload Your Song', ['class' => 'custom-file-label']) }}<br />
+				{{ Form::file('audiofile_dir', ['class' => 'custom-file-input']) }}
+			</div>
+			<br /><br />
 
-		<div class="form-group">
-			{{ Form::hidden('_method', 'PUT') }}
-			{{ Form::submit('Submit', ['class' => 'btn btn-primary', 'id' => 'btn']) }}
-		</div>
-		
-	{!! Form::close() !!}
+			<div class="form-group">
+				{{ Form::hidden('_method', 'PUT') }}
+				{{ Form::submit('Submit', ['class' => 'btn btn-primary', 'id' => 'btn']) }}
+			</div>
+			
+		{!! Form::close() !!}
+	</div>
+	
 
 	<script type="text/javascript">
 		$(document).ready(function(){
